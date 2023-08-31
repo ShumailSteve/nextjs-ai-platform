a2604a39c5df131637f3fa13df9f6781c332e787
ShumailSteve <shumailsteve@gmail.com>
2023-09-01 02:32:15 +0500

    initial commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+
+const DashboardPage = () => {
+    return (
+      <div>
+        <p className="text-5xl">Dashboard Page (Protected)</p>
+      </div>
+   )
+  }
+  
+export default DashboardPage
