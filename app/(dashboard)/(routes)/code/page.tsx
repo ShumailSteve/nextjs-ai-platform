0eea0d5a3a6a58faa1fa3bacb8b2a09126f1cb1d
ShumailSteve <shumailsteve@gmail.com>
2023-10-15 23:59:21 +0500

    code generation added

---
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -0,0 +1,150 @@
+"use client"
+
+import { useState } from "react"
+import { Code } from "lucide-react"
+import * as z from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import axios from 'axios'
+import { useRouter } from "next/navigation"
+import { ChatCompletionRequestMessage } from "openai"
+import ReactMarkdown from "react-markdown"
+
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form"
+
+import Heading from "@/components/heading"
+import { UserAvatar } from "@/components/user-avatar"
+import { BotAvatar } from "@/components/bot-avatar"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Empty } from "@/components/empty"
+import { Loader } from "@/components/loader";
+import { cn } from "@/lib/utils";
+import { formSchema } from "./constants"
+
+const CodePage  = () => {
+    const router = useRouter()
+    const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([])
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            prompt: ""
+        }
+    })
+
+    const isLoading = form.formState.isSubmitting
+
+    const submitHandler = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const userMessage: ChatCompletionRequestMessage = {
+                role: "user",
+                content: values.prompt,
+            }
+
+            const newMessage = [...messages, userMessage]
+
+            const res = await axios.post("/api/code", {
+                messages: newMessage
+            })
+
+            setMessages((current) => [...current, userMessage, res.data])
+
+            form.reset()
+        } catch (err) {
+            console.log(err)
+        } finally {
+            router.refresh()
+        }   
+    } 
+
+  return (
+    <div>
+        <Heading
+            title="Code Generation"
+            description="The most advanced code model."
+            icon={Code}
+            iconColor="text-green-700"
+            bgColor="bg-green-700/10"
+        />
+        <div className="px-4 lg:px-8">
+            <div>
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(submitHandler)}
+                        className="rounded-lg border w-full p-4 px-3 md:px-6
+                           focus-within:shadow-sm grid grid-cols-12 gap-2
+                        "
+                    >
+                        <FormField
+                            name="prompt"
+                            render={({ field }) => (
+                                <FormItem className="col-span-12 lg:col-span-10">
+                                    <FormControl className="m-0 p-0">
+                                        <Input 
+                                            className="border-0 outline-none 
+                                            focus-visible:ring-0 focus-visible:ring-transparent"
+                                            disabled={isLoading}
+                                            placeholder="Explain useEffect hook in react js"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        <Button className="col-span-12 lg:col-span-2">
+                            Generate
+                        </Button>
+                    </form>
+                </Form>
+            </div>
+            <div className="space-y-4 mt-4">
+                { isLoading && (
+                    <div className="p-8 rounded-lg w-dull flex items-center justify-center bg-muted">
+                        <Loader />
+                    </div>
+                )}
+                {messages.length === 0 && !isLoading && (
+                    <Empty label="No Conversation started." />
+                )}
+                <div className="flex flex-col-reverse gap-y-4">
+                    {messages.map((message) => (
+                        <div 
+                            key={message.content}
+                            className={cn(
+                                "p-8 w-full flex items-start gap-x-8 rounded-lg",
+                                message.role === "user" 
+                                  ? "bg-white border border-black/10"
+                                  : "bg-muted"
+                            )}
+                        >
+                            {message.role === "user" ? <UserAvatar /> : <BotAvatar /> }
+                            <ReactMarkdown
+                                className="text-sm overflow-hidden leading-7"
+                                components={{
+                                    pre: ({ node, ...props }) => (
+                                        <div 
+                                            className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg"
+                                        >
+                                            <pre {...props} />
+                                        </div>
+                                    ),
+                                    code: ({ node, ...props }) => (
+                                        <code 
+                                            className="bg-black/10 p-1 rounded-lg" 
+                                            {...props} 
+                                        />
+                                    ),
+                                }}
+                            >
+                                {message.content || ""}
+                            </ReactMarkdown>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CodePage 
---
+++ app/api/code/route.ts
@@ -0,0 +1,46 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration)
+
+const instructionMessage: ChatCompletionRequestMessage = {
+  role: "system",
+  content: "You are a code generator. You must generate performance efficient answer only in markdown code snippets."
+}
+
+export async function POST(req: Request) {
+    try {
+        const { userId } = auth()
+        const body = await req.json()
+        const { messages } = body
+
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+          }
+      
+          if (!configuration.apiKey) {
+            return new NextResponse("OpenAI API Key not configured.", { status: 500 });
+          }
+      
+          if (!messages) {
+            return new NextResponse("Messages are required", { status: 400 });
+          }
+      
+          const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [instructionMessage, ...messages]
+          });
+
+          return NextResponse.json(response.data.choices[0].message);
+
+    } catch (err) {
+        console.log("Code_ERROR: ", err)
+        return new NextResponse("Internal server  error", { status: 500 })
+    }
+}
+
